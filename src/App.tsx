df0fd12fa5e39f7e4716a8e46e2a56e8b5a5c5b2
sujan pal <sujanpal79@gmail.com>
2025-11-14 11:57:17 +0530

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-
-// import './App.css';
-import Calendar from './components/Calendar/Calendar';
+import React from "react";
+import Calendar from "./components/Calendar/Calendar";
 
 function App() {
-    function dateYMD(y: number, m: number, d: number) {
-    return new Date(y, m - 1, d); // so you can use a 1-based month
-  }
-  const sample = dateYMD(2025, 11, 16);
+  const dateYMD = (y: number, m: number, d: number) => {
+    return new Date(y, m - 1, d);
+  };
+  const dateSample = dateYMD(2025, 11, 16);
   return (
     <div className="App">
-      <Calendar date={sample} />
+      <Calendar date={dateSample} />
     </div>
   );
 }
